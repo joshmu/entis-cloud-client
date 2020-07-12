13575b904895f92a8dad73d398a32199ad992c65
Josh Mu <mu@joshmu.com>
2020-07-13 09:44:15 +1000

    blank

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import React, { useState } from 'react'
-import './App.less'
-import { Layout } from 'antd'
-import MainLayout from './components/MainLayout.js'
-import SidebarLayout from './components/SidebarLayout.js'
+import React from 'react'
+import './App.css'
 
 function App() {
-  const [state, setState] = useState({ collapsed: false })
-
-  /** @param {boolean} collapsed */
-  const onCollapse = collapsed => {
-    console.log(collapsed)
-    setState({ collapsed })
-  }
-
-  return (
-    <div className='App'>
-      <Layout style={{ minHeight: '100vh' }}>
-        <SidebarLayout collapsed={state.collapsed} onCollapse={onCollapse} />
-        <MainLayout />
-      </Layout>
-    </div>
-  )
+  return <div className='App'>hello</div>
 }
 
 export default App
