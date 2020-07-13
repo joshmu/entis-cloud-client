9df69291a629e7fb89ea4fbe6541db8f7712af94
Josh Mu <mu@joshmu.com>
2020-07-14 09:44:41 +1000

    db.json for my-json-server

--- src/components/Tanks/Tanks.js
+++ src/components/Tanks/Tanks.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { makeStyles, useTheme, Grid, Paper } from '@material-ui/core'
 import Title from '../Elements/Title'
 import LiquidGauge from './LiquidGauge'
+import { useGlobalContext } from '../../contexts/globalContext'
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -13,19 +14,21 @@ const useStyles = makeStyles(theme => ({
 }))
 
 export default function Tanks() {
+  const { db } = useGlobalContext()
+
   const theme = useTheme()
   const classes = useStyles()
 
   const tanks = new Array(4).fill({}).map((t, i) => {
-    return { title: `Tank ${i + 1}`, value: Math.random() * 100 }
+    return { name: `Tank ${i + 1}`, value: Math.random() * 100 }
   })
 
   return (
     <>
-      {tanks.map((tank, index) => (
+      {db.slice(0, 4).map((tank, index) => (
         <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
           <Paper className={classes.paper}>
-            <Title>{tank.title}</Title>
+            <Title>{tank.name}</Title>
             <LiquidGauge
               value={tank.value}
               radius={100}
--- src/contexts/globalContext.js
+++ src/contexts/globalContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext } from 'react'
+import React, { createContext, useState, useEffect, useContext } from 'react'
 
 const initialContext = {}
 
@@ -6,8 +6,18 @@ export const globalContext = createContext(initialContext)
 
 export const GlobalProvider = ({ children }) => {
   const [pageTitle, setPageTitle] = useState('Hello World')
+  const [db, setDb] = useState([])
+
+  useEffect(() => {
+    if (db.length === 0) {
+      fetch('https://my-json-server.typicode.com/joshmu/entis-cloud-client')
+        .then(response => response.json())
+        .then(data => setDb(data))
+    }
+  }, [db])
+
   return (
-    <globalContext.Provider value={{ pageTitle, setPageTitle }}>
+    <globalContext.Provider value={{ pageTitle, setPageTitle, db }}>
       {children}
     </globalContext.Provider>
   )
