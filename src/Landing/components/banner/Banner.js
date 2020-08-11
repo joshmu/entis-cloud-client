2c81127725dbe3fdec801a20711a8b93165e3746
Josh Mu <mu@joshmu.com>
2020-08-12 09:39:43 +1000

    :nails: banner size on small

--- src/Landing/components/banner/Banner.js
+++ src/Landing/components/banner/Banner.js
@@ -4,7 +4,7 @@ import honeywellImg from './assets/honeywell.png'
 const Banner = () => {
   return (
     <div className='flex justify-center items-center bg-white py-4'>
-      <div className='w-1/2 md:w-1/3'>
+      <div className='w-2/3 sm:w-1/2 md:w-1/3'>
         <img
           src={honeywellImg}
           alt='honeywell company logo banner'
