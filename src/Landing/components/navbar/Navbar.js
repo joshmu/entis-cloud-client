95ed7e84667b8bc59ffa74a1cce09ccb66dd7cd8
Josh Mu <mu@joshmu.com>
2020-08-11 13:41:39 +1000

    :nails: tailwind navbar

--- src/Landing/components/navbar/Navbar.js
+++ src/Landing/components/navbar/Navbar.js
@@ -28,25 +28,34 @@ const Navbar = () => {
   }
 
   return (
-    <AppBar position='relative'>
-      <Toolbar>
-        <CloudQueueIcon className={classes.icon} />
-        <Typography
-          variant='h6'
-          color='inherit'
-          noWrap
-          className={classes.title}
-        >
-          {title}
-        </Typography>
-        <Button color='inherit' onClick={handleLogin}>
-          Login
-        </Button>
-        <Button color='inherit' onClick={handleRegister}>
-          Register
-        </Button>
-      </Toolbar>
-    </AppBar>
+    <>
+      <div className='font-bold bg-indigo-700 flex justify-between items-center text-white px-4 md:px-6 shadow-xl'>
+        <div id='logo' className='text-xl capitalize flex items-center py-4'>
+          <CloudQueueIcon className='fill-current' />
+          <h1 className='ml-2'>{title}</h1>
+        </div>
+        <div id='menu'>
+          <ul className='flex'>
+            <li>
+              <button
+                className='hover:text-gray-100 focus:outline-none uppercase font-semibold cursor-pointer px-2 py-1'
+                onClick={handleLogin}
+              >
+                Login
+              </button>
+            </li>
+            <li>
+              <button
+                className='hover:text-gray-100 focus:outline-none uppercase font-semibold cursor-pointer px-2 py-1'
+                onClick={handleRegister}
+              >
+                Register
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   )
 }
 
