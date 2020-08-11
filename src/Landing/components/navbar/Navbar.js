9fab9af025bbbdc13c3e47567487d4b3ddff7184
Josh Mu <mu@joshmu.com>
2020-08-11 13:43:00 +1000

    :hammer: navbar

--- src/Landing/components/navbar/Navbar.js
+++ src/Landing/components/navbar/Navbar.js
@@ -1,25 +1,12 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography, Button } from '@material-ui/core'
 import { CloudQueue as CloudQueueIcon } from '@material-ui/icons'
-import { makeStyles } from '@material-ui/core/styles'
 import { useGlobalContext } from '../../../contexts/globalContext'
 
-const useStyles = makeStyles(theme => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}))
-
 const Navbar = () => {
-  const classes = useStyles()
   const { setOpenLoginDialog, setOpenRegisterDialog } = useGlobalContext()
 
   const title = 'Entis Cloud'
 
-  // todo: finish login button for landing appbar
   const handleLogin = () => {
     setOpenLoginDialog(true)
   }
@@ -28,34 +15,32 @@ const Navbar = () => {
   }
 
   return (
-    <>
-      <div className='font-bold bg-indigo-700 flex justify-between items-center text-white px-4 md:px-6 shadow-xl'>
-        <div id='logo' className='text-xl capitalize flex items-center py-4'>
-          <CloudQueueIcon className='fill-current' />
-          <h1 className='ml-2'>{title}</h1>
-        </div>
-        <div id='menu'>
-          <ul className='flex'>
-            <li>
-              <button
-                className='hover:text-gray-100 focus:outline-none uppercase font-semibold cursor-pointer px-2 py-1'
-                onClick={handleLogin}
-              >
-                Login
-              </button>
-            </li>
-            <li>
-              <button
-                className='hover:text-gray-100 focus:outline-none uppercase font-semibold cursor-pointer px-2 py-1'
-                onClick={handleRegister}
-              >
-                Register
-              </button>
-            </li>
-          </ul>
-        </div>
+    <div className='font-bold bg-indigo-700 flex justify-between items-center text-white px-4 md:px-6 shadow-xl'>
+      <div id='logo' className='text-xl capitalize flex items-center py-4'>
+        <CloudQueueIcon className='fill-current' />
+        <h1 className='ml-2'>{title}</h1>
       </div>
-    </>
+      <div id='menu'>
+        <ul className='flex'>
+          <li>
+            <button
+              className='hover:text-gray-100 focus:outline-none uppercase font-semibold cursor-pointer px-2 py-1'
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+          </li>
+          <li>
+            <button
+              className='hover:text-gray-100 focus:outline-none uppercase font-semibold cursor-pointer px-2 py-1'
+              onClick={handleRegister}
+            >
+              Register
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
