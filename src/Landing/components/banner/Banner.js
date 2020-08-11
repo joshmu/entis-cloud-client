c4b107e81b8022b94332e0393a20dd8523c0cc0f
Josh Mu <mu@joshmu.com>
2020-08-11 17:01:13 +1000

    :sparkles: tailwind banner

--- src/Landing/components/banner/Banner.js
+++ src/Landing/components/banner/Banner.js
@@ -1,30 +1,18 @@
 import React from 'react'
-import { Container, Grid } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
 import honeywellImg from './assets/honeywell.png'
 
-const useStyles = makeStyles(theme => ({
-  container: {
-    paddingTop: theme.spacing(4),
-    backgroundColor: theme.palette.background.paper,
-  },
-}))
-
 const Banner = () => {
-  const classes = useStyles()
   return (
-    <Container className={classes.container} maxWidth={false}>
-      <Grid container justify='center'>
-        <Grid item xs={8} sm={6} md={4}>
-          <img
-            src={honeywellImg}
-            alt='honeywell company logo banner'
-            height='100%'
-            width='100%'
-          />
-        </Grid>
-      </Grid>
-    </Container>
+    <div className='flex justify-center items-center bg-white py-4'>
+      <div className='w-1/2 md:w-1/3'>
+        <img
+          src={honeywellImg}
+          alt='honeywell company logo banner'
+          height='100%'
+          width='100%'
+        />
+      </div>
+    </div>
   )
 }
 
